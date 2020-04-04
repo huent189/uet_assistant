240ea50cd35a61896c53075d8a365b93b016b00f
Tung Lam <tungtobi@gmail.com>
2020-04-04 15:44:13 +0700

    basic dashboard scene

--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/LoginActivity.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/LoginActivity.java
@@ -2,7 +2,9 @@ package vnu.uet.mobilecourse.assistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(LoginActivity.this, MyCoursesActivity.class);
+        startActivity(intent);
     }
 }
