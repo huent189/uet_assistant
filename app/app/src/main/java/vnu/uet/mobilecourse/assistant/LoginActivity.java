99048e39534792041cccf97f1e5456aaaee43c1a
Tung Lam <tungtobi@gmail.com>
2020-04-04 14:35:51 +0700

    login activity

---
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/LoginActivity.java
@@ -0,0 +1,14 @@
+package vnu.uet.mobilecourse.assistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
