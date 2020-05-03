bc91cd67208aa35f4432777aba83943643ac0ea2
huent189 <hueahgase189@gmail.com>
2020-05-03 15:40:45 +0700

    Merge branch 'dev-lamnt' of https://github.com/huent189/uet_assistant into dev-huent
    # Conflicts:
    #	app/app/build.gradle

--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/model/Token.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/model/Token.java
@@ -2,6 +2,7 @@ package vnu.uet.mobilecourse.assistant.model;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import vnu.uet.mobilecourse.assistant.view.MyApplication;
 
 public class Token {
     private static final String preferenceName = "UET_ASSITANT";
@@ -15,16 +16,21 @@ public class Token {
 
     public static String getToken() {
         //TODO: intergation test
-//        if(token == null){
-//            token = preferences.getString("TOKEN", null);
-//        }
+        if(token == null){
+            preferences = MyApplication.getInstance().getSharedPreferences(preferenceName, Context.MODE_PRIVATE);
+            token = preferences.getString("TOKEN", null);
+        }
+
         return token;
     }
 
     public static void setToken(String token) {
         Token.token = token;
         //TODO: intergation test
-//        preferences.edit().putString("TOKEN", token).apply();
+        if (preferences == null){
+            preferences = MyApplication.getInstance().getSharedPreferences(preferenceName, Context.MODE_PRIVATE);
+            preferences.edit().putString("TOKEN", token).apply();
+        }
     }
     // for testing only
     public static void clearToken(){
