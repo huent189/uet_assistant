2c0c248a91ff962b104e29d7c104cb5239b8033c
huent189 <hueahgase189@gmail.com>
2020-05-05 17:29:10 +0700

    Merge branch 'dev-haunt'

--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/repository/UserRepository.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/repository/UserRepository.java
@@ -1,20 +1,13 @@
 package vnu.uet.mobilecourse.assistant.repository;
 
 import android.util.Log;
-import com.google.gson.JsonObject;
-import android.content.Intent;
-import android.util.Log;
-import android.view.View;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.gson.JsonObject;
 import retrofit2.Call;
 import vnu.uet.mobilecourse.assistant.SharedPreferencesManager;
 import vnu.uet.mobilecourse.assistant.network.HTTPClient;
