0436043d4fa6ee60fefe1cb55312cad3ac20eb0b
huent189 <hueahgase189@gmail.com>
2020-05-13 22:20:52 +0700

    add check login

--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/network/HTTPClient.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/network/HTTPClient.java
@@ -1,37 +1,55 @@
 package vnu.uet.mobilecourse.assistant.network;
 
 import android.util.Log;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import okhttp3.*;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import vnu.uet.mobilecourse.assistant.model.User;
+import vnu.uet.mobilecourse.assistant.model.WeeklyMaterial;
+import vnu.uet.mobilecourse.assistant.network.deserializer.CourseContentDeserializer;
 
 import java.io.IOException;
 
 public class HTTPClient {
-    private static Retrofit coursesClient;
+    private Retrofit coursesClient;
     private static final String COURSES_BASE_URL = "https://courses.uet.vnu.edu.vn";
-    public static Retrofit getCoursesClient() {
-        if (coursesClient == null) {
-            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+    private Gson gson;
+    private static HTTPClient instance;
+    private HTTPClient(){
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .addInterceptor(new Interceptor() {
-                @Override
-                public Response intercept(Chain chain) throws IOException {
-                    Request original = chain.request();
-                    Log.d("COURSES", "intercept: " + User.getInstance().getToken());
-                    HttpUrl url = original.url().newBuilder().addQueryParameter("moodlewsrestformat", "json")
-                                    .addQueryParameter("wstoken", User.getInstance().getToken())
-                                    .build();
-                    Request request = original.newBuilder().url(url).build();
-                    return chain.proceed(request);
-                }
+                    @Override
+                    public Response intercept(Chain chain) throws IOException {
+                        Request original = chain.request();
+                        Log.d("COURSES", "intercept: " + User.getInstance().getToken());
+                        HttpUrl url = original.url().newBuilder().addQueryParameter("moodlewsrestformat", "json")
+                                .addQueryParameter("wstoken", User.getInstance().getToken())
+                                .build();
+                        Request request = original.newBuilder().url(url).build();
+                        return chain.proceed(request);
+                    }
                 }).build();
-            coursesClient = new retrofit2.Retrofit.Builder()
-                    .client(okHttpClient)
-                    .baseUrl(COURSES_BASE_URL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(WeeklyMaterial[].class, new CourseContentDeserializer());
+        gson = gsonBuilder.create();
+        coursesClient = new retrofit2.Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(COURSES_BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
+    public static HTTPClient getInstance(){
+        if(instance == null){
+            instance = new HTTPClient();
         }
-        return coursesClient;
+        return instance;
+    }
+    public <T> T request(final Class<T> service){
+        return coursesClient.create(service);
+    }
+    public Gson getGson() {
+        return gson;
     }
 }
