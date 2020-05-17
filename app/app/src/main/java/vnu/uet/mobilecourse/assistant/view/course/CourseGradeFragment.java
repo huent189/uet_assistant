118d132f031250a863fa144d405951d706244111
Tung Lam <tungtobi@gmail.com>
2020-05-17 16:35:45 +0700

    merge course grade scene

--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/view/course/CourseGradeFragment.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/view/course/CourseGradeFragment.java
@@ -1,22 +1,29 @@
 package vnu.uet.mobilecourse.assistant.view.course;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import vnu.uet.mobilecourse.assistant.R;
 import vnu.uet.mobilecourse.assistant.adapter.GradeAdapter;
+import vnu.uet.mobilecourse.assistant.model.Grade;
 import vnu.uet.mobilecourse.assistant.viewmodel.CourseGradeViewModel;
 
 public class CourseGradeFragment extends Fragment {
@@ -24,6 +31,8 @@ public class CourseGradeFragment extends Fragment {
     private CourseGradeViewModel viewModel;
 
     private GradeAdapter gradeAdapter;
+
+    private int courseId;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -34,21 +43,67 @@ public class CourseGradeFragment extends Fragment {
 
         initializeGradeListView(root);
 
+        TextView tvGradeProgress = root.findViewById(R.id.tvGradeProgress);
+
+        ShimmerFrameLayout shimmerTvGradeProgress = root.findViewById(R.id.shimmerTvGradeProgress);
+        shimmerTvGradeProgress.startShimmerAnimation();
+
+        viewModel.getCourseSummaryGrade(courseId).observe(getViewLifecycleOwner(), new Observer<Grade>() {
+            @Override
+            public void onChanged(Grade grade) {
+                if (grade == null) {
+                    shimmerTvGradeProgress.setVisibility(View.VISIBLE);
+                    tvGradeProgress.setVisibility(View.INVISIBLE);
+
+                } else {
+                    tvGradeProgress.setVisibility(View.VISIBLE);
+                    shimmerTvGradeProgress.setVisibility(View.INVISIBLE);
+
+                    String gradeProgress = String.format(Locale.ROOT,
+                            "%.0f/%.0f",
+                            grade.getUserGrade(),
+                            grade.getMaxGrade());
+
+                    tvGradeProgress.setText(gradeProgress);
+                }
+            }
+        });
+
         return root;
     }
 
     private void initializeGradeListView(View root) {
-        List<String> testes = new ArrayList<>();
-
-        for (int i = 1; i < 20; i++) {
-            testes.add("Bài kiểm tra " + i);
-        }
-
-        gradeAdapter = new GradeAdapter(testes, this);
-
         RecyclerView rvGrades = root.findViewById(R.id.rvGrades);
-
-        rvGrades.setAdapter(gradeAdapter);
         rvGrades.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        // get bundle from prev fragment
+        Bundle args = getArguments();
+
+        if (args != null) {
+            // get course id from bundle
+            courseId = args.getInt("courseId");
+
+            Fragment thisFragment = this;
+
+            ShimmerFrameLayout shimmerRvGrades = root.findViewById(R.id.shimmerRvGrades);
+            shimmerRvGrades.startShimmerAnimation();
+
+            viewModel.getCourseGrades(courseId).observe(getViewLifecycleOwner(), new Observer<List<Grade>>() {
+                @Override
+                public void onChanged(List<Grade> grades) {
+                    if (grades == null || grades.isEmpty()) {
+                        shimmerRvGrades.setVisibility(View.VISIBLE);
+                        rvGrades.setVisibility(View.INVISIBLE);
+
+                    } else {
+                        shimmerRvGrades.setVisibility(View.INVISIBLE);
+                        rvGrades.setVisibility(View.VISIBLE);
+
+                        gradeAdapter = new GradeAdapter(grades, thisFragment);
+                        rvGrades.setAdapter(gradeAdapter);
+                    }
+                }
+            });
+        }
     }
 }
--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/viewmodel/CourseGradeViewModel.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/viewmodel/CourseGradeViewModel.java
@@ -1,7 +1,22 @@
 package vnu.uet.mobilecourse.assistant.viewmodel;
 
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
+import vnu.uet.mobilecourse.assistant.model.Grade;
+import vnu.uet.mobilecourse.assistant.repository.CourseRepository;
 
 public class CourseGradeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
+    private CourseRepository courseRepo = CourseRepository.getInstance();
+
+    public LiveData<List<Grade>> getCourseGrades(int courseId) {
+        return courseRepo.getGrades(courseId);
+    }
+
+    public LiveData<Grade> getCourseSummaryGrade(int courseId) {
+        return courseRepo.getTotalGrades(courseId);
+    }
+
 }
