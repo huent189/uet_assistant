9066e32da120bfe7fcda32c5af69da80a066f04c
tungtobi <tungtobi99>
2020-05-20 17:38:16 +0700

    alarm manager

--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/view/MainActivity.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/view/MainActivity.java
@@ -1,12 +1,17 @@
 package vnu.uet.mobilecourse.assistant.view;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import vnu.uet.mobilecourse.assistant.R;
+import vnu.uet.mobilecourse.assistant.receiver.AlarmReceiver;
 import vnu.uet.mobilecourse.assistant.repository.FirebaseAuthenticationService;
 import vnu.uet.mobilecourse.assistant.repository.UserRepository;
 import vnu.uet.mobilecourse.assistant.viewmodel.state.StateModel;
@@ -25,12 +30,14 @@ public class MainActivity extends AppCompatActivity {
             public void onChanged(StateModel<String> stateModel) {
                 switch (stateModel.getStatus()) {
                     case SUCCESS:
-                        boolean isFirebaseLoggedIn = FirebaseAuthenticationService.isFirebaseLoggedIn();
+//                        boolean isFirebaseLoggedIn = FirebaseAuthenticationService.isFirebaseLoggedIn();
+//
+//                        if (isFirebaseLoggedIn)
+//                            navigateToActivity(MyCoursesActivity.class);
+//                        else
+//                            navigateToActivity(LoginFirebaseActivity.class);
 
-                        if (isFirebaseLoggedIn)
-                            navigateToActivity(MyCoursesActivity.class);
-                        else
-                            navigateToActivity(LoginFirebaseActivity.class);
+                        testAlarm();
 
                         break;
 
@@ -47,5 +54,29 @@ public class MainActivity extends AppCompatActivity {
     private void navigateToActivity(Class<? extends AppCompatActivity> activity) {
         Intent intent = new Intent(MainActivity.this, activity);
         startActivity(intent);
+    }
+
+    private void testAlarm() {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Context context = getApplicationContext();
+
+        Intent intent = new Intent(context, AlarmReceiver.class);
+
+        intent.setAction("remind");
+        intent.putExtra("data", "test");
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        long ALARM_DELAY_IN_SECOND = 50;
+        long alarmTimeUTC = System.currentTimeMillis() + ALARM_DELAY_IN_SECOND;
+
+        assert alarmManager != null;
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, alarmTimeUTC, "pendingIntent", new AlarmManager.OnAlarmListener() {
+            @Override
+            public void onAlarm() {
+                Toast.makeText(context, "data", Toast.LENGTH_LONG).show();
+            }
+        }, null);
     }
 }
