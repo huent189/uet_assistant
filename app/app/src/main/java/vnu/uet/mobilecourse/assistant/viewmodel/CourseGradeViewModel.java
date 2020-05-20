2c21172d7a78eb52333da0cc4bcb05e57895c30a
tungtobi <tungtobi99>
2020-05-20 12:36:07 +0700

    refactor

--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/viewmodel/CourseGradeViewModel.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/viewmodel/CourseGradeViewModel.java
@@ -9,14 +9,14 @@ import vnu.uet.mobilecourse.assistant.repository.CourseRepository;
 
 public class CourseGradeViewModel extends ViewModel {
 
-    private CourseRepository courseRepo = CourseRepository.getInstance();
+    private CourseRepository mCourseRepo = CourseRepository.getInstance();
 
     public LiveData<List<Grade>> getCourseGrades(int courseId) {
-        return courseRepo.getGrades(courseId);
+        return mCourseRepo.getGrades(courseId);
     }
 
     public LiveData<Grade> getCourseSummaryGrade(int courseId) {
-        return courseRepo.getTotalGrades(courseId);
+        return mCourseRepo.getTotalGrades(courseId);
     }
 
 }
