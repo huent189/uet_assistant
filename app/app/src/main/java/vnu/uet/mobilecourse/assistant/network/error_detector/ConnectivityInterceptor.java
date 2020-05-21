1868aa85ffb7a8fb400d59fc841a5a1bd7a16572
huent189 <hueahgase189@gmail.com>
2020-05-21 16:31:34 +0700

    tmp

---
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/network/error_detector/ConnectivityInterceptor.java
@@ -0,0 +1,4 @@
+package vnu.uet.mobilecourse.assistant.network.error_detector;
+
+public class ConnectivityInterceptor {
+}
--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/view/MainActivity.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/view/MainActivity.java
@@ -1,16 +1,15 @@
 package vnu.uet.mobilecourse.assistant.view;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import vnu.uet.mobilecourse.assistant.R;
+import vnu.uet.mobilecourse.assistant.repository.FirebaseAuthenticationService;
+import vnu.uet.mobilecourse.assistant.repository.UserRepository;
+import vnu.uet.mobilecourse.assistant.viewmodel.state.StateModel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,32 +25,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void accessCourses(View view) {
         mBtnAccess.setActivated(false);
-        navigateToActivity(MyCoursesActivity.class);
+//        navigateToActivity(MyCoursesActivity.class);
 
 
-//        new UserRepository().isLoggedIn().observe(MainActivity.this, new Observer<StateModel<String>>() {
-//            @Override
-//            public void onChanged(StateModel<String> stateModel) {
-//                switch (stateModel.getStatus()) {
-//                    case SUCCESS:
-//                        boolean isFirebaseLoggedIn = FirebaseAuthenticationService.isFirebaseLoggedIn();
-//
-//                        if (isFirebaseLoggedIn)
-//                            navigateToActivity(MyCoursesActivity.class);
-//                        else
-//                            navigateToActivity(LoginFirebaseActivity.class);
-//
-//                        break;
-//
-//                    case ERROR:
-//                        stateModel.getError().printStackTrace();
-//                        navigateToActivity(LoginActivity.class);
-//
-//                        break;
-//
-//                }
-//            }
-//        });
+        new UserRepository().isLoggedIn().observe(MainActivity.this, new Observer<StateModel<String>>() {
+            @Override
+            public void onChanged(StateModel<String> stateModel) {
+                switch (stateModel.getStatus()) {
+                    case SUCCESS:
+                        boolean isFirebaseLoggedIn = FirebaseAuthenticationService.isFirebaseLoggedIn();
+
+                        if (isFirebaseLoggedIn)
+                            navigateToActivity(MyCoursesActivity.class);
+                        else
+                            navigateToActivity(LoginFirebaseActivity.class);
+
+                        break;
+
+                    case ERROR:
+                        stateModel.getError().printStackTrace();
+                        navigateToActivity(LoginActivity.class);
+
+                        break;
+
+                }
+            }
+        });
     }
 
     private void navigateToActivity(Class<? extends AppCompatActivity> activity) {
