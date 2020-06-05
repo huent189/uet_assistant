a67a0240c5a951ec4d2938247298b524c9d3e384
Tung Lam <tungtobi@gmail.com>
2020-06-05 10:13:46 +0700

    [update] my profile view with fb

--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/MyProfileViewModel.java
+++
@@ -1,7 +0,0 @@
-package vnu.uet.mobilecourse.assistant;
-
-import androidx.lifecycle.ViewModel;
-
-public class MyProfileViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/view/profile/MyProfileFragment.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/view/profile/MyProfileFragment.java
@@ -1,19 +1,25 @@
 package vnu.uet.mobilecourse.assistant.view.profile;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import vnu.uet.mobilecourse.assistant.MyProfileViewModel;
+import vnu.uet.mobilecourse.assistant.viewmodel.MyProfileViewModel;
 import vnu.uet.mobilecourse.assistant.R;
+import vnu.uet.mobilecourse.assistant.model.firebase.UserInfo;
+import vnu.uet.mobilecourse.assistant.util.DateTimeUtils;
+import vnu.uet.mobilecourse.assistant.viewmodel.state.StateModel;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class MyProfileFragment extends Fragment {
 
@@ -26,6 +32,31 @@ public class MyProfileFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(MyProfileViewModel.class);
 
         View root = inflater.inflate(R.layout.fragment_my_profile, container, false);
+
+        TextView tvDoB = root.findViewById(R.id.tvDoB);
+        TextView tvClass = root.findViewById(R.id.tvClass);
+        TextView tvUsername = root.findViewById(R.id.tvUsername);
+        TextView tvUserId = root.findViewById(R.id.tvUserId);
+
+        mViewModel.getUserInfo().observe(getViewLifecycleOwner(), new Observer<StateModel<UserInfo>>() {
+            @Override
+            public void onChanged(StateModel<UserInfo> stateModel) {
+                switch (stateModel.getStatus()) {
+                    case SUCCESS:
+                        UserInfo userInfo = stateModel.getData();
+
+                        tvUsername.setText(userInfo.getName());
+                        tvUserId.setText(userInfo.getId());
+
+                        Date dob = new Date(userInfo.getDOB());
+                        tvDoB.setText(DateTimeUtils.DATE_FORMAT.format(dob));
+
+                        tvClass.setText(userInfo.getUetClass());
+
+                        break;
+                }
+            }
+        });
 
         return root;
     }
