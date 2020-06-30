f63ace62bab960fd6b569af08647be9c9c215d4f
haupc <haupc.uet@gmail.com>
2020-06-30 15:29:16 +0700

    change avatar

--- app/app/src/main/java/vnu/uet/mobilecourse/assistant/database/Storage.java
+++ app/app/src/main/java/vnu/uet/mobilecourse/assistant/database/Storage.java
@@ -1,5 +1,74 @@
 package vnu.uet.mobilecourse.assistant.database;
 
-public class Storage {
+import android.net.Uri;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+
+
+import vnu.uet.mobilecourse.assistant.viewmodel.state.IStateLiveData;
+import vnu.uet.mobilecourse.assistant.viewmodel.state.StateLiveData;
+import vnu.uet.mobilecourse.assistant.viewmodel.state.StateModel;
+import vnu.uet.mobilecourse.assistant.viewmodel.state.StateStatus;
+
+interface IStorageInterface {
+
+    /**
+     *
+     * @param groupId id of group chat
+     * @param localPath path to local file
+     * @return
+     */
+    IStateLiveData<String> uploadFileToGroupChat(String groupId, String localPath);
+
+    /**
+     *
+     * @param Id id of user or group
+     * @param localPath path to file in local
+     * @return LiveData contains path to avatar on cloud
+     */
+    IStateLiveData<String> changeAvatar(String Id, String localPath);
+
+    StorageReference getAvatar(String Id);
+
+}
+
+public class Storage implements IStorageInterface {
+    public static final String AVATAR_DIR = "AVATAR";
+    public static final String GROUP_DIR = "GROUP";
+    public static final String AVATAR_FILENAME = "avatar.jpg";
+    StorageReference storage = FirebaseStorage.getInstance().getReference();
+
+    @Override
+    public IStateLiveData<String> uploadFileToGroupChat(String groupId, String localPath) {
+        return null;
+    }
+
+    @Override
+    public IStateLiveData<String> changeAvatar(String Id, String localPath) {
+        IStateLiveData<String> changeAvatarState = new StateLiveData<>(new StateModel<>(StateStatus.LOADING));
+        Uri fileURI = Uri.fromFile(new File(localPath));
+        // delete old avatar
+        StorageReference avatarRef = storage.child(Storage.AVATAR_DIR).child(Id).child(Storage.AVATAR_FILENAME);
+        avatarRef.delete().addOnSuccessListener(aVoid -> { // delete old avatar
+            avatarRef.putFile(fileURI).addOnCompleteListener(task -> { // upload new avatar
+                if (task.isSuccessful()) {
+                    changeAvatarState.postSuccess(avatarRef.getPath());
+                } else {
+                    changeAvatarState.postError(task.getException());
+                }
+            });
+        });
+
+        return changeAvatarState;
+    }
+
+    @Override
+    public StorageReference getAvatar(String Id) {
+        return storage.child(Storage.AVATAR_DIR).child(Id).child(Storage.AVATAR_FILENAME);
+    }
+
 
 }
